d1a6030b75765e8b25777b526cd04702c7808c3f
Dan Powell <dan@autumnrain.co.uk>
2016-02-12 13:43:43 +0000

    Tweaked tests some more

--- src/tests/functional/shop/cartItems/AddCartWithExtrasCept.php
+++ src/tests/functional/shop/cartItems/AddCartWithExtrasCept.php
@@ -7,9 +7,15 @@ $I->wantTo('Add a product to cart that has extras');
 // Create a Product and add some Extras
 $product = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
 for($i = 0; $i < 2; $i++) {
-    $extras[] = $I->makeModel(DanPowell\Shop\Models\Extra::class, [], null, 2);
+    $extras[] = $I->makeModel(DanPowell\Shop\Models\Extra::class, [], 'inStock', 1);
 }
 $product->extras()->saveMany($extras);
+
+// Create another product so we can try and force it's option on to the 'correct' product
+$unrelatedProduct = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
+$unrelatedExtra = $I->makeModel(DanPowell\Shop\Models\Extra::class, [], 'inStock', 1);
+$unrelatedProduct->extras()->save($unrelatedExtra);
+
 
 // Go to the product page
 $I->amOnRoute('shop.product.show', $product->slug);
@@ -17,14 +23,20 @@ $I->amOnRoute('shop.product.show', $product->slug);
 // Select only the first Extra
 $I->checkOption('extra[' . $extras[0]->id . ']');
 
-$I->submitForm('#addToCart', []);
+$I->submitForm('#addToCart', [
+    // include an unrelated extra
+    'extra[' . $unrelatedExtra->id . ']' => 'on'
+]);
 $I->seeCurrentRouteIs('shop.cart.index');
 
 $I->see('Product added to cart', '.alert');
 $I->see($product->title, '.CartTable-product-title');
+
 // Should see first Extra
 $I->see($extras[0]->title, '.CartTable-item-extras');
+
 // Should *not* see any other Extras
 $I->dontSee($extras[1]->title, '.CartTable-item-extas');
+$I->dontSee($unrelatedExtra->title, '.CartTable-item-extas');
 
 ?>
--- src/tests/functional/shop/cartItems/AddCartWithOptionsCept.php
+++ src/tests/functional/shop/cartItems/AddCartWithOptionsCept.php
@@ -4,16 +4,23 @@ $I = new FunctionalTester($scenario);
 
 $I->wantTo('Add a product to cart that has options');
 
+// Make models
+// Create a product with some options
 $product = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
-
 $options[] = $I->makeModel(DanPowell\Shop\Models\Option::class, [], 'radio', 1);
 $options[] = $I->makeModel(DanPowell\Shop\Models\Option::class, [], 'select', 1);
 $options[] = $I->makeModel(DanPowell\Shop\Models\Option::class, [], 'text', 1);
-
 $product->options()->saveMany($options);
+
+// Create another product so we can try and force it's option on to the 'correct' product
+$unrelatedProduct = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
+$unrelatedOption = $I->makeModel(DanPowell\Shop\Models\Option::class, [], 'select', 1);
+$unrelatedProduct->options()->save($unrelatedOption);
+
 
 $I->amOnRoute('shop.product.show', $product->slug);
 
+// Randomly choose the option values
 $option_values = [];
 foreach($options as $option) {
     if($option->type == 'radio' || $option->type == 'select') {
@@ -25,18 +32,26 @@ foreach($options as $option) {
     }
 }
 
-$I->submitForm('#addToCart', []);
+// Submit the form
+$I->submitForm('#addToCart', [
+    // include an unrelated option
+    'option[' . $unrelatedOption->id . ']' => $unrelatedOption->config[0]
+]);
+
 $I->seeCurrentRouteIs('shop.cart.index');
+
+// Check that we see the product
 $I->see('Product added to cart', '.alert');
 $I->see($product->title, '.CartTable-product-title');
 
+// Check that the selected options have been sent through
 foreach($options as $option) {
     $I->see($option->title, '.CartTable-item-options');
-    if($option->type == 'radio' || $option->type == 'select') {
-        $I->see($option_values[$option->id], '.CartTable-item-options');
-    } else {
-        $I->see('Testing', '.CartTable-item-options');
-    }
+    $I->see($option_values[$option->id], '.CartTable-item-options');
 }
+
+// Check that the unrelated option we forced through has *not* appeared
+$I->dontSee($unrelatedOption->title, '.CartTable-item-options');
+$I->dontSee($unrelatedOption->config[0], '.CartTable-item-options');
 
 ?>
