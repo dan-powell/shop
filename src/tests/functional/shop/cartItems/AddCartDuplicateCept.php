6472711040d1d9190a1f865589b9dd91e354396e
Dan Powell <dan@autumnrain.co.uk>
2016-02-18 13:23:22 +0000

    More testes

---
+++ src/tests/functional/shop/cartItems/AddCartDuplicateCept.php
@@ -0,0 +1,27 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+
+$I->wantTo('Add a duplicate product to cart');
+
+$product = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
+
+$I->amOnRoute('shop.product.show', $product->slug);
+
+$I->submitForm('#addToCart', [
+    'quantity' => 2
+]);
+
+$I->seeCurrentRouteIs('shop.cart.index');
+
+$I->amOnRoute('shop.product.show', $product->slug);
+
+$I->submitForm('#addToCart', [
+    'quantity' => 2
+]);
+
+$I->seeCurrentRouteIs('shop.cart.index');
+
+$I->see('x4');
+
+
---
+++ src/tests/functional/shop/cartItems/AddCartLowStockCept.php
@@ -0,0 +1,19 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+
+$I->wantTo('Add a product to cart that has low stock');
+
+$product = $I->createModel(DanPowell\Shop\Models\Product::class, [], 'inStock', 1);
+
+$I->amOnRoute('shop.product.show', $product->slug);
+
+$I->submitForm('#addToCart', [
+    'quantity' => 15
+]);
+
+$I->dontSee('Product added to cart');
+
+$I->see('not enough product stock');
+
+$I->seeFormHasErrors();
